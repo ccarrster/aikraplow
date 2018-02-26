0372b108a0addcdfb99489cb2df7b4693c13154a
Chris <chris.carr@cobersolutions.com>
2018-02-26 18:15:40 -0500

    Tests

---
+++ TestApiStorage.php
@@ -0,0 +1,66 @@
+<?php
+
+require_once("iApiStorage.php");
+
+class TestApiStorage implements iApiStorage{
+
+	var $games = [];
+
+	function createGame(){
+		$game = [];
+		$game["state"] = "new";
+		$game["id"] = count($this->games);
+		$game["clients"] = [];
+		$this->games[] = $game;
+		return $game["id"];
+	}
+
+	function getGameState($gameId){
+		foreach($this->games as $game){
+			if($gameId == $game["id"]){
+				return $game["state"];
+			}
+		}
+	}
+
+	function isClientInGame($gameId, $clientId){
+		foreach($this->games as $game){
+			if($gameId == $game["id"]){
+				foreach($game["clients"] as $client){
+					if($client == $clientId){;
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	function addClientToGame($gameId, $clientId){
+		foreach($this->games as &$game){
+			if($gameId == $game["id"]){
+				foreach($game["clients"] as $client){
+					if($client == $clientId){
+						return false;
+					}
+				}
+				$game["clients"][] = $clientId;
+				return true;
+			}
+		}
+	}
+
+	function removeClientFromGame($gameId, $clientId){
+		foreach($this->games as &$game){
+			if($gameId == $game["id"]){
+				foreach($game["clients"] as $key=>$client){
+					if($client == $clientId){
+						unset($game["clients"][$key]);
+						return true;
+					}
+				}
+				return false;
+			}
+		}
+	}
+}
--- test/APITest.php
+++ test/APITest.php
@@ -3,8 +3,108 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
-final class EmailTest extends TestCase
+require_once("KraplowAPI.php");
+require_once("TestApiStorage.php");
+
+final class APITest extends TestCase
 {
-	 public function testSomething(){
+	 public function testCreateGame(){
+	 	$testStorage = new TestApiStorage();
+		$sut = new KraplowAPI($testStorage);
+		$_GET["action"] = "newgame";
+		$this->assertEquals(0, count($testStorage->games));
+		ob_start();
+		$sut->handleAction();
+		$output = ob_get_flush();
+		$this->assertEquals(0, $output);
+		$this->assertEquals(1, count($testStorage->games));
+		$sut->handleAction();
+		$this->assertEquals(2, count($testStorage->games));
+	 }
+
+	 public function testJoinGame(){
+		$testStorage = new TestApiStorage();
+		$sut = new KraplowAPI($testStorage);
+		$_GET["action"] = "newgame";
+		$sut->handleAction();
+		$_GET["action"] = "joingame";
+		$_GET["gameid"] = "0";
+		$_GET["clientid"] = "1";
+		$this->assertEquals(0, count($testStorage->games[0]["clients"]));
+		ob_start();
+		$sut->handleAction();
+		$output = ob_get_flush();
+		$this->assertEquals("true", $output);
+		$this->assertEquals(1, count($testStorage->games[0]["clients"]));
+
+		ob_start();
+		$sut->handleAction();
+		$output = ob_get_flush();
+		$this->assertEquals('"Can not join"', $output);
+
+		$_GET["action"] = "joingame";
+		$_GET["gameid"] = "0";
+		$_GET["clientid"] = "2";
+		$sut->handleAction();
+		$this->assertEquals(2, count($testStorage->games[0]["clients"]));
+	 }
+
+	 public function testLeaveGame(){
+		$testStorage = new TestApiStorage();
+		$sut = new KraplowAPI($testStorage);
+		$_GET["action"] = "newgame";
+		$sut->handleAction();
+		$_GET["action"] = "joingame";
+		$_GET["gameid"] = "0";
+		$_GET["clientid"] = "1";
+		$sut->handleAction();
+		$this->assertEquals(1, count($testStorage->games[0]["clients"]));
+		$_GET["action"] = "leavegame";
+		$_GET["gameid"] = "0";
+		$_GET["clientid"] = "1";
+		$sut->handleAction();
+		$this->assertEquals(0, count($testStorage->games[0]["clients"]));
+	 }
+
+	 public function testLeaveGameWrongId(){
+		$testStorage = new TestApiStorage();
+		$sut = new KraplowAPI($testStorage);
+		$_GET["action"] = "newgame";
+		$sut->handleAction();
+		$_GET["action"] = "joingame";
+		$_GET["gameid"] = "0";
+		$_GET["clientid"] = "1";
+		$sut->handleAction();
+		$this->assertEquals(1, count($testStorage->games[0]["clients"]));
+		$_GET["action"] = "leavegame";
+		$_GET["gameid"] = "0";
+		$_GET["clientid"] = "2";
+		$sut->handleAction();
+		$this->assertEquals(1, count($testStorage->games[0]["clients"]));
+	 }
+
+	 public function testLeaveGameBadCall(){
+	 	unset($_GET);
+		$testStorage = new TestApiStorage();
+		$sut = new KraplowAPI($testStorage);
+		$_GET["action"] = "newgame";
+		$sut->handleAction();
+		$_GET["action"] = "joingame";
+		$_GET["gameid"] = "0";
+		$sut->handleAction();
+		$this->assertEquals(0, count($testStorage->games[0]["clients"]));
+	 }
+
+	  public function testJoinGameBadCall(){
+		unset($_GET);
+		$testStorage = new TestApiStorage();
+		$sut = new KraplowAPI($testStorage);
+		$_GET["action"] = "newgame";
+		$sut->handleAction();
+		$_GET["action"] = "joingame";
+		$_GET["clientid"] = "1";
+		$this->assertEquals(0, count($testStorage->games[0]["clients"]));
+		$sut->handleAction();
+		$this->assertEquals(0, count($testStorage->games[0]["clients"]));
 	 }
 }
